d648c84c0c82b784451479397db6a77299df2d2f
rachel <165401157@qq.com>
2024-12-16 14:24:14 +0800

    add

---
+++ l1/solidity/contracts/ContractB.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+contract ContractB {
+    uint256 public value;
+    function setValue(uint256 _value) public {
+        value = _value;
+    }
+    function getValue() public view returns (uint256) {
+        return value;
+    }
+}
